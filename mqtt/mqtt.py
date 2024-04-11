0f9ac359352f61c57790d88dd0c6fed7b5aa838d
chengningly <114815243+Aliancn@users.noreply.github.com>
2024-04-11 21:05:43 +0800

    添加针对不同topic的处理函数，可直接在函数内执行对应的逻辑

--- mqtt/mqtt.py
+++ mqtt/mqtt.py
@@ -3,20 +3,35 @@ import paho.mqtt.client as mqtt
 from django.conf import settings
 
 topic = [("django/helmet/test1", 1), ("django/helmet/test2", 1)]
+
+
 def on_connect(mqtt_client, userdata, flags, rc):
     if rc == 0:
         print('Connected successfully')
         mqtt_client.subscribe(topic)
-    else:       
+    else:
         print('Bad connection. Code:', rc)
+
+def func1(payload):
+    print('Function 1')
+
+def func2(payload):
+    print('Function 2')
 
 
 def on_message(mqtt_client, userdata, msg):
     # print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
     msg.payload = msg.payload.decode('utf-8')
     payload_json = json.loads(msg.payload)
-    print(f'Received message on topic: {msg.topic} with payload: {payload_json}, {type(payload_json)}')
-
+    print(
+        f'Received message on topic: {msg.topic} with payload: {payload_json}, {type(payload_json)}')
+    # 多重选择语句
+    # 按照topic的不同，执行不同的函数
+    switch = {
+        "topic1": func1,
+        "topic2": func2
+    }
+    switch[msg.topic](payload_json)
 
 client = mqtt.Client()
 client.on_connect = on_connect
